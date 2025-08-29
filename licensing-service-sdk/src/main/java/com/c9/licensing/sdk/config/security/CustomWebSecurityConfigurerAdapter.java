a11ddab6f8ec2d3456c568dd363b862b825b7ab0
bsayli <baris.sayli@gmail.com>
2025-08-29 17:24:14 -0600

    Readme.md and docker_volumes.zip were updated. Readme.md constructions are now working. keycloak  Host port was changed from 8080 → 8180

--- licensing-service-sdk/src/main/java/com/c9/licensing/sdk/config/security/CustomWebSecurityConfigurerAdapter.java
+++ licensing-service-sdk/src/main/java/com/c9/licensing/sdk/config/security/CustomWebSecurityConfigurerAdapter.java
@@ -1,5 +1,6 @@
 package com.c9.licensing.sdk.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,6 +29,7 @@ public class CustomWebSecurityConfigurerAdapter {
     this.authenticationEntryPoint = authenticationEntryPoint;
   }
 
+  @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder)
       throws Exception {
     auth.inMemoryAuthentication()
